557f08681bdad0f47dce74fcb561580ab173d90b
Michael Lombard <miklombard@yahoo.com>
2023-05-23 15:22:12 -0400

    first commit

---
+++ src/00-objectball.js
@@ -0,0 +1,14 @@
+function gameObject(){
+    const obj = {
+        home: {
+            teamName: 
+            colors: 
+            players: 
+
+
+        }
+        away: {} 
+    }
+    return obj;
+
+}
